89be47129d7dfedaf960a6966f2bd3f2d2103a76
flyyuan <740004544@qq.com>
2020-12-18 11:18:31 +0800

    feat: 抽出 config

--- emp-config.js
+++ emp-config.js
@@ -2,11 +2,11 @@ const withVue3 = require('@efox/emp-vue3')
 const path = require('path')
 const ProjectRootPath = path.resolve('./')
 const { getConfig } = require(path.join(ProjectRootPath, './src/config'))
+const empConfig = require(`${resolveApp('')}/emp.json`);
 module.exports = withVue3(({ config, env, empEnv }) => {
   const confEnv = env === 'production' ? 'prod' : 'dev'
   const conf = getConfig(empEnv || confEnv)
   const port = conf.port
-  const projectName = 'vue3Template'
   const publicPath = conf.publicPath
   // 设置项目URL
   config.output.publicPath(publicPath)
@@ -16,21 +16,11 @@ module.exports = withVue3(({ config, env, empEnv }) => {
     args[0] = {
       ...args[0],
       ...{
-        // 项目名称
-        name: projectName,
+        ...empConfig,
         // 暴露项目的全局变量名
-        library: { type: 'var', name: projectName },
+        library: { type: 'var', name: empConfig.name },
         // 被远程引入的文件名
         filename: 'emp.js',
-        remotes: {
-          // 远程项目别名:远程引入的项目名
-        },
-        // 需要暴露的东西
-        exposes: {
-          // 别名:组件的路径
-        },
-        // 需要共享的依赖
-        shared: [],
       },
     }
     return args
